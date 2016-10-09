634d5eca3bd4c9514de2a96e36c79d0e685640f5
Soham Das <soham5959595@gmail.com>
2016-10-09 16:32:24 +0530

    2nd
    completed project

--- js/working.js
+++ js/working.js
@@ -1,94 +1,90 @@
 $(function() {
 
-  var player = 1;
-  var table = $('table');
-  var text = $('#text');
-  var turn = $('#turn');
-  displayNextPlayer(turn, player);
+  var player=1;
+  var table=$('table');
+  var turn=$('.turn');
+  var text=$('.text');
 
   $('td').click(function() {
-    td = $(this);
-    var state = getState(td);
-    if(!state) {
-      var pattern = definePatternForCurrentPlayer(player);
-      changeState(td, pattern);
-      if(checkIfPlayerWon(table, pattern)) {
-        text.html('Player '+player+' has won.');
-        turn.html('');
-      } else {
-        player = setNextPlayer(player);
-        displayNextPlayer(turn, player);
-      }
-    } else {
-      text.html('This box is already checked.');
+    var td=$(this);
+    var state=getState(td);
+    if (state) {
+      text.html('Box already checked !');
     }
-  });
+    else {
+      var ele=create(td);
+      var win;
+      td.addClass(ele);
+      win=getResult(win, ele, table);
+      if(win) {
+        text.html('');
+        turn.html('PLAYER '+player+' HAS WON !!');
+      }
+      else {
+        if (player==1) {
+          player=2;
+        }
+        else {
+          player=1;
+        }
+        text.html('');
+        turn.html('Player turn - '+player);
+      }
+    }
+  })
 
-  $('.reset').click(function() {
-    player = 1;
+  $('.res').click(function() {
+    player=1;
+    turn.html('Player 1 start the game.');
     text.html('');
     reset(table);
-    displayNextPlayer(turn, player);
-  });
+  })
+  function getState(td) {
+    if(td.hasClass('cross')||td.hasClass('circle'))
+      return 1;
+    else
+      return 0;
+  }
 
-});
+  function create(td) {
+    if(player==1)
+      return 'cross';
+    else {
+      return 'circle';
+    }
+  }
 
-function getState(td) {
-  if(td.hasClass('cross') || td.hasClass('circle')) {
-    return 1;
-  } else {
+  function getResult(win, ele, table) {
+    if (table.find('#b1').hasClass(ele)&&table.find('#b2').hasClass(ele)&&table.find('#b3').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b4').hasClass(ele)&&table.find('#b5').hasClass(ele)&&table.find('#b6').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b7').hasClass(ele)&&table.find('#b8').hasClass(ele)&&table.find('#b9').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b1').hasClass(ele)&&table.find('#b4').hasClass(ele)&&table.find('#b7').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b2').hasClass(ele)&&table.find('#b5').hasClass(ele)&&table.find('#b8').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b3').hasClass(ele)&&table.find('#b6').hasClass(ele)&&table.find('#b9').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b3').hasClass(ele)&&table.find('#b5').hasClass(ele)&&table.find('#b7').hasClass(ele)) {
+      return 1;
+    }
+    else if (table.find('#b1').hasClass(ele)&&table.find('#b5').hasClass(ele)&&table.find('#b9').hasClass(ele)) {
+      return 1;
+    }
     return 0;
   }
-}
 
-function changeState(td, pattern) {
-  return td.addClass(pattern);
-}
-
-function definePatternForCurrentPlayer(player) {
-  if(player == 1) {
-    return 'cross';
-  } else {
-    return 'circle';
+  function reset(table) {
+    table.find('td').each(function() {
+      $(this).removeClass('cross').removeClass('circle');
+    })
   }
-}
-
-function setNextPlayer(player) {
-  if(player == 1) {
-    return player = 2;
-  } else {
-    return player = 1;
-  }
-}
-
-function displayNextPlayer(turn, player) {
-  turn.html('Player turn : '+player);
-}
-
-function checkIfPlayerWon(table, pattern) {
-  var won = 0;
-  if(table.find('#b1').hasClass(pattern) && table.find('#b2').hasClass(pattern) && table.find('#b3').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b1').hasClass(pattern) && table.find('#b4').hasClass(pattern) && table.find('#b7').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b1').hasClass(pattern) && table.find('#b5').hasClass(pattern) && table.find('#b9').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b4').hasClass(pattern) && table.find('#b5').hasClass(pattern) && table.find('#b6').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b7').hasClass(pattern) && table.find('#b8').hasClass(pattern) && table.find('#b9').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b2').hasClass(pattern) && table.find('#b5').hasClass(pattern) && table.find('#b8').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b3').hasClass(pattern) && table.find('#b6').hasClass(pattern) && table.find('#b9').hasClass(pattern)) {
-    won = 1;
-  } else if (table.find('#b3').hasClass(pattern) && table.find('#b5').hasClass(pattern) && table.find('#b7').hasClass(pattern)) {
-    won = 1;
-  }
-  return won;
-}
-
-function reset(table) {
-  table.find('td').each(function() {
-    $(this).removeClass('circle').removeClass('cross');
-  });
-}
+})
